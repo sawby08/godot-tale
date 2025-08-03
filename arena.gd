89755ef0f2d598b783db402625dec6b554e1de99
Sawby <130822964+sawby08@users.noreply.github.com>
2025-08-03 03:11:29 -0400

    improve collision and arena drawing

--- arena.gd
+++ arena.gd
@@ -8,25 +8,15 @@ func _ready() -> void:
 		Vector2(-283.0, -65.0),
 		Vector2(-283.0, 65.0),
 		Vector2(282.0, 65.0),
-		Vector2(282.0, -65.0)
+		Vector2(282.0, -65.0),
+		Vector2(-283.0, -65.0),
 	]
 
 func _process(delta: float) -> void:
-	var black_outline_verts = []
-	var white_outline_verts = []
-	
-	for i in range(verts.size()):
-		var v = verts[i]
-		
-		var x_sign = 1 if v.x > 0 else -1
-		var y_sign = 1 if v.y > 0 else -1
-		var black_offset = Vector2(7 * x_sign, 7 * y_sign)
-		var white_offset = Vector2(5 * x_sign, 5 * y_sign)
-
-		black_outline_verts.append(v + black_offset)
-		white_outline_verts.append(v + white_offset)
-
-	$BlackOutline.polygon = black_outline_verts
-	$WhiteOutline.polygon = white_outline_verts
-	$Fill.polygon = verts
 	$Collision.polygon = verts
+	queue_redraw()
+
+func _draw() -> void:
+	draw_polyline(verts, Color.BLACK, 14)
+	draw_polyline(verts, Color.WHITE, 10)
+	draw_polygon(verts, [Color.BLACK])
